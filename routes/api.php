2e6712b0d4f7f07c0118591f4b14d6dab395a18e
Samad4509 <samad4509@gmail.com>
2025-10-04 13:04:56 +0600

    last update

--- routes/api.php
+++ routes/api.php
@@ -87,7 +87,7 @@ Route::middleware(['agent', 'agent.approved'])->prefix('agent')->group(function 
 //     Route::get('/agent/reset_password/{token}/{email}', [AgentController::class, 'reset_password'])->name('agent.reset_password');
 //     Route::post('/agent/reset_password_submit',[AgentController::class,'reset_password_submit'])->name('agent.reset_password_submit');
     
-//LAST UPDATE
+//LAST UPDATE 2025
      
 
 // });
